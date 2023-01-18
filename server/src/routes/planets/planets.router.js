76e47c668fbfcde761ee6d712b75531997463f73
Azher Uddin <azher305uddin@gmail.com>
2023-01-18 18:28:41 +0600

    feat: project with client

---
+++ server/src/routes/planets/planets.router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const { getPlanets } = require('./planets.controller');
+
+const router = express.Router();
+
+router.get('/', getPlanets);
+
+module.exports = router;
